d34620a8bdc536782be7626861183a0d9df766b2
zhuohui-xhh <1181754773@qq.com>
2024-05-28 14:20:04 +0800

    路径别名配置（默认配置了src）并做尝试

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,10 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
+      // 配置scr路径别名
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
