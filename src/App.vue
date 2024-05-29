d508c8404384295ca4154eb9d80512377190c3ab
zhuohui-xhh <1181754773@qq.com>
2024-05-29 17:09:34 +0800

    elementPlus全局引入

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,7 @@ import HelloWorld from '@/components/HelloWorld.vue'
       <HelloWorld msg="You did it!" />
 
       <nav>
+        <el-button>Default</el-button>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav>
